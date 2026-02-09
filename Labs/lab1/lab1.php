f715499e8f0425b999a297db6334647ed0cc2a0a
David Panesiu <davidpanesiu@gmail.com>
2026-02-08 22:30:58 -0500

    fix: remove unecessary link for font awesome icons on lab 1 page and update title

--- Labs/lab1/lab1.php
+++ Labs/lab1/lab1.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="noindex, nofollow" />
   <link rel="stylesheet" href="../../styles.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>Lab 1 - Christmas Trees</title>
+  <title>Lab 1</title>
   <style>
     .tree-container {
       line-height: 0.5;
